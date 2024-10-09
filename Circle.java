d664faf52d1b1201ad6f11e0f8a5f3b59c347657
Admin <quangphanthanhnguyen@gmail.com>
2024-10-09 23:09:48 +0700

    1.1

--- Circle.java
+++ Circle.java
@@ -1,30 +1,43 @@
 public class Circle{
     private double radius = 1;
+    private String color = "red";
 
     public Circle(){
+        radius = 1;
+        color = "red";
     }
 
     public Circle(double radius){
         this.radius = radius;
+        color = "red";
+    }
+
+    public Circle(double radius, String color){
+        this.radius = radius;
+        this.color = color;
     }
 
     public double getRadius(){
         return radius;
     }
-    
+
+    public String getColor(){
+        return color;
+    }
+
     public void setRadius(double radius){
         this.radius = radius;
     }
 
-    public double getArea(){
-        return Math.PI*radius*radius;
-    }
-
-    public double getCircumference(){
-        return 2*Math.PI*radius;
+    public void setColor(String color){
+        this.color = color;
     }
 
     public String toString(){
-        return "Circle[radius = " + radius + "]";
+        return "Circle[radius = " + radius + ", color = " + color + "]";
+    }
+
+    public double getArea(){
+        return Math.PI*radius*radius;
     }
 }
--- TestCircle.java
+++ TestCircle.java
@@ -1,15 +1,18 @@
-public class TestCircle {
-   public static void main(String[] args) {
-      Circle c1 = new Circle(2);
-      System.out.println(c1);
-      Circle c2 = new Circle();
-      System.out.println(c2);
-      System.out.println("----------");
-      c1.setRadius(4);
-      System.out.println(c1);
-      System.out.println("----------");
-      System.out.println(c1.getRadius());
-      System.out.println(c1.getArea());
-      System.out.println(c1.getCircumference());
-   }
+public class TestCircle{
+    public static void main(String[]args){
+        Circle c1 = new Circle();
+        Circle c2 = new Circle(10);
+        Circle c3 = new Circle(5, "white");
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        c3.setColor("blue");
+        c2.setRadius(4);
+        System.out.println("------------");
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println("------------");
+        System.out.println(c2.getArea());
+        System.out.println(c3.getArea());
+    }
 }
